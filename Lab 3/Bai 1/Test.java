2a9331e038c41fe7bf71aeb0ca8c4e1058077855
quangdang46 <tranquangdang21@gmail.com>
2022-09-14 20:44:45 +0700

    bai 1

--- Lab 3/Bai 1/Test.java
+++ Lab 3/Bai 1/Test.java
@@ -1,45 +1,66 @@
 public class Test {
   public static int factorial(int n) {
-    if (n == 0 || n == 1)
+    if (n == 0 || n == 1) {
       return 1;
-    return n * factorial(n - 1);
+    }
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+
   }
 
   public static int compute(int x, int n) {
-    if (n == 0)
+    if (n == 0) {
       return 1;
-    return x * compute(x, n - 1);
+    }
+    int result = x;
+    for (int i = 1; i < n; i++) {
+      result *= x;
+    }
+    return result;
   }
 
   public static int countDigits(int n) {
-    if (n == 0)
-      return 0;
-    return 1 + countDigits(n / 10);
+    return Integer.toString(n).length();
   }
 
-  // isprime recursion
-  public static boolean isPrime(int n, int i) {
-    if (n <= 2)
-      return (n == 2) ? true : false;
-    if (n % i == 0)
+  public static boolean isPrime(int n) {
+    if (n <= 2) {
+      return n == 2 ? true : false;
+    }
+    if (n % 2 == 0) {
       return false;
-    if (i * i > n)
-      return true;
-    return isPrime(n, i + 1);
+    }
+    for (int i = 3; i * i <= n; i += 2) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
   }
 
   public static int gcd(int a, int b) {
-    if (b == 0)
-      return a;
-    return gcd(b, a % b);
+    if (a == 0 || b == 0) {
+      return a + b;
+    }
+
+    while (a != b) {
+      if (a > b) {
+        a -= b;
+      } else {
+        b -= a;
+      }
+    }
+    return a;
   }
 
   public static void main(String[] args) {
     System.out.println(factorial(5));
     System.out.println(compute(2, 3));
     System.out.println(countDigits(12345));
-    System.out.println(isPrime(5, 2));
-    System.out.println(gcd(18, 12));
-
+    System.out.println(isPrime(7));
+    System.out.println(gcd(12, 18));
   }
 }
