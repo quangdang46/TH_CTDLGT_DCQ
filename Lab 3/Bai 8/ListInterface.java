7677b4417f12119ce2abb28313d892a6ed1dd678
quangdang46 <tranquangdang21@gmail.com>
2022-09-15 20:32:31 +0700

    bai 8

---
+++ Lab 3/Bai 8/ListInterface.java
@@ -0,0 +1,5 @@
+public interface ListInterface<E> {
+  public E removeCurr(Node<E> curr);
+
+  public void addSortedList(E item, Node<E> curr);
+}
---
+++ Lab 3/Bai 8/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+  public static void main(String[] args) {
+    MyLinkedList<Integer> list = new MyLinkedList<Integer>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    //error
+    list.addSortedList(5, list.getFirst());
+
+    list.print();
+    System.out.println();
+    System.out.println("count " + list.countEven(list.getFirst()));
+    System.out.println("sum " + list.sumLinkedList(list.getFirst()));
+  }
+}
