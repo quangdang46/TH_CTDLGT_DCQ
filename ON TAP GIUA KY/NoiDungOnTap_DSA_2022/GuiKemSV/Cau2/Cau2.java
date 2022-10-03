a0bf3ec6ca84bf284f372b4c2f9f797f84b38e2b
quangdang46 <tranquangdang21@gmail.com>
2022-10-04 05:57:03 +0700

    cau 2

--- ON TAP GIUA KY/NoiDungOnTap_DSA_2022/GuiKemSV/Cau2/Cau2.java
+++ ON TAP GIUA KY/NoiDungOnTap_DSA_2022/GuiKemSV/Cau2/Cau2.java
@@ -1,9 +1,12 @@
 public class Cau2 {
-    public static int recur(int n, int k) {
-        // code here
-    }
+	public static int recur(int n, int k) {
+		if (k == 0) {
+			return 1;
+		}
+		return n * recur(n, k - 1);
+	}
 
-    public static void main(String args[]){
-		
+	public static void main(String args[]) {
+		System.out.println(recur(5, 5));
 	}
 }
