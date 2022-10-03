6b72a9df99c0550e85ce903b0f6448e1cfa0db63
quangdang46 <tranquangdang21@gmail.com>
2022-10-04 06:06:43 +0700

    full

--- ON TAP GIUA KY/NoiDungOnTap_DSA_2022/GuiKemSV/Cau3/Cau3.java
+++ ON TAP GIUA KY/NoiDungOnTap_DSA_2022/GuiKemSV/Cau3/Cau3.java
@@ -1,11 +1,35 @@
 import java.util.Stack;
 
 public class Cau3 {
-    public static int calculate(String[] expression) {
-        // code here
-    }
+	public static int solve(String c, int a, int b) {
+		switch (c) {
+			case "+":
+				return a + b;
+			case "-":
+				return a - b;
+		}
+		return 0;
+	}
 
-    public static void main(String args[]){
-		System.out.println(calculate(new String[]{"31", "12", "+"}));
+	private static boolean isNumber(String str) {
+		return str.matches("0|([1-9][0-9]*)");
+	}
+
+	public static int calculate(String[] expression) {
+		Stack<Integer> stack = new Stack<>();
+		for (String number : expression) {
+			if (isNumber(number)) {
+				stack.push(Integer.parseInt(number));
+			} else {
+				int a = stack.pop();
+				int b = stack.pop();
+				stack.push(solve(number, a, b));
+			}
+		}
+		return stack.pop();
+	}
+
+	public static void main(String args[]) {
+		System.out.println(calculate(new String[] { "31", "12", "+" }));
 	}
 }
